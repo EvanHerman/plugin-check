8f081fa3423e4ca25fecc7a9cf146f8be8d21423
Evan Herman <evan.m.herman@gmail.com>
2023-03-29 04:13:05 -0400

    add build scripts

---
+++ bin/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rm -rf build/*
+
+# This script is used to build the project.
+mkdir -p build/plugin-check/ >/dev/null 2>&1
+
+# Copy the plugin files to the build directory
+cp class-plugin-check.php build/plugin-check/class-plugin-check.php
+cp -r test-results/ build/plugin-check/test-results/
+cp readme.txt build/plugin-check/readme.txt
+cp LICENSE build/plugin-check/LICENSE
+
+cp -r includes/ build/plugin-check/includes/
+cp -r bin/ build/plugin-check/bin/
+
+# Remove what we don't need
+rm -f build/plugin-check/bin/plugin-scan/README.md
+rm -rf build/plugin-check/test-results/*
+
+# ZIP the plugin
+zip -r build/plugin-check.zip build/plugin-check/
