969317bfd72eb56e3a1e5147e8308433f6a42bd3
Evan Herman <evan.m.herman@gmail.com>
2023-03-29 17:12:33 -0400

    fix sed for gh actions

--- bin/version.sh
+++ bin/version.sh
@@ -8,12 +8,12 @@ echo "Bumping the version to $VERSION"
 # Replace the plugin version header.
 # Version: 1.0.0
 # perl -i.bak -pe 's{(Version:\s+)\d+[.]\d+[.]\d+}{$1$ENV{VERSION}}g' class-plugin-check.php
-sed -i '' 's/Version: .*/Version: '"$VERSION"'/' class-plugin-check.php
+sed -i 's/Version: .*/Version: '"$VERSION"'/' class-plugin-check.php
 
 # Replace the plugin version constant.
 # define( 'WP_PLUGIN_CHECK_VERSION', '1.0.0' );
-sed -i '' "s/define( 'WP_PLUGIN_CHECK_VERSION', .*/define( 'WP_PLUGIN_CHECK_VERSION', '"$VERSION"' );/" class-plugin-check.php
+sed -i "s/define( 'WP_PLUGIN_CHECK_VERSION', .*/define( 'WP_PLUGIN_CHECK_VERSION', '"$VERSION"' );/" class-plugin-check.php
 
 # Replace the plugin version in the README.md file.
 # # WordPress Plugin Check v0.0.1
-sed -i '' "s/# WordPress Plugin Check .*/# WordPress Plugin Check v"$VERSION"/" README.md
+sed -i "s/# WordPress Plugin Check .*/# WordPress Plugin Check v"$VERSION"/" README.md
