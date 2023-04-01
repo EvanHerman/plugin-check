78ad6a09564e3b987b4673ef02c275c2662357e9
Evan Herman <evan.m.herman@gmail.com>
2023-04-01 03:00:58 -0400

    Add phpcs exclusion for PHP8+

--- bin/build.sh
+++ bin/build.sh
@@ -24,6 +24,10 @@ rm -f build/plugin-check/**/.DS_Store
 # Remove the bash check in the plugin scan (doesn't work locally for me)
 sed -i '8,14d' 'build/plugin-check/bin/plugin-scan/plugin-scan.sh'
 
+# Add an exclusion for PHP 8+ to the plugin-scan phpcs.xml file at line 32
+sed -i '32i\
+<ini name="error_reporting" value="E_ALL &#38; ~E_DEPRECATED" />' bin/plugin-scan/plugin-scan.sh.xml
+
 chmod +x build/plugin-check/bin/plugin-scan/plugin-scan.sh
 chmod +x build/plugin-check/bin/plugin-scan/plugin-scan.sh.ignore
 chmod +x build/plugin-check/bin/plugin-scan/plugin-scan.sh.xml
